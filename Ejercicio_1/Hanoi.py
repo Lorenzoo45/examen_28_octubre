c3561fcd54f43272779cb9957f7485d006574fea
lorenzoo22 <lagudmarr@gmail.com>
2022-10-28 15:48:31 +0200

    1er Ejercicio

---
+++ Ejercicio_1/Hanoi.py
@@ -0,0 +1,15 @@
+def moverTorre(altura,origen, destino, intermedio):
+    if altura >= 1:
+        moverTorre(altura-1,origen,intermedio,destino)
+        moverDisco(origen,destino)
+        moverTorre(altura-1,intermedio,destino,origen)
+
+def moverDisco(desde,hacia,contador=0):
+    print("mover disco de",desde,"a",hacia)
+    contador += 1
+    print("movimientos totales:",contador)
+
+# moverTorre(3,"A","B","C")# Ejemplo sencillo con solo tres agujas
+moverTorre(64,"A","B","C")# Ejemplo sencillo con solo tres agujas
+
+#El número de movimientos necesarios para mover correctamente una torre de 64 discos es 264−1
